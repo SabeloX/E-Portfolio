40d9c92f37f053d3ee1c6f932caf99b6f42cb733
Sabelo <sabelomtw@gmail.com>
2021-09-03 18:22:05 +0200

    Added animation on the profile image

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import profileImage from '../../assets/DSC_7953.jpg';
 import './dashboard.css';
+import styled, { keyframes } from 'styled-components';
+import { fadeInLeftBig } from 'react-animations';
+
+const bounceAnimation = keyframes`${fadeInLeftBig}`;
+
+const Bouncy = styled.div`
+    animation: 2s ${bounceAnimation};
+    width: 400px;
+`;
 
 const Dashboard = () => {
     return (
         <div className="dashboard_container">
-            <div className="profile_image_container">
+            <Bouncy>
                 <img className="profile_image" src={profileImage} alt="Sabelo"/>
-            </div>
+            </Bouncy>
         </div>
     )
 }
