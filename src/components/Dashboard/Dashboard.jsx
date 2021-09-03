6c54e59e030a20a27fef844500d30224386f2c2b
Sabelo <sabelomtw@gmail.com>
2021-09-03 11:46:32 +0200

    Profile Image on Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import profileImage from '../../assets/DSC_7953.jpg';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h2>Dashboard</h2>
+        <div className="dashboard">
+            <img className="profile_image" src={profileImage} alt="Sabelo"/>
         </div>
     )
 }
 
-export default Dashboard
+export default Dashboard;
