498005dcf9b0475b7f0cd27b304074611ef2b539
Sabelo <sabelomtw@gmail.com>
2021-09-03 20:01:17 +0200

    Added dashboad comp in router

--- src/components/Root/App.jsx
+++ src/components/Root/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Router>
+        <Switch>
+          <Route path='/' component={Dashboard}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
