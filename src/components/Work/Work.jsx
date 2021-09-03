5af403c900e28984947b867cb2a523ece5c1f0aa
Sabelo <sabelomtw@gmail.com>
2021-09-03 21:09:31 +0200

    Added images of works

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import "./work.css";
+import varsityEats from '../../assets/varsityeatsuct.co.za_(Nexus 5X).png';
+import workout from '../../assets/tinder-clone-75b11.web.app_signin(Nexus 5X) (1).png';
+import tikTok from '../../assets/tik-tok-f6910.web.app_(Nexus 5X).png';
+
+const works = [
+    {
+        name: 'Varsity Eats',
+        image: varsityEats,
+        url: 'https://varsityeatsuct.co.za/'
+    },
+    {
+        name: 'Workout Plan',
+        image: workout,
+        url: 'https://tinder-clone-75b11.web.app/'
+    },
+    {
+        name: 'TikTok Clone',
+        image: tikTok,
+        url: 'https://tik-tok-f6910.web.app/'
+    }
+];
 
 const Work = () => {
     return (
         <div className="work_container">
-            <h2>Work</h2>
+            <div className="projects">
+                <h1>Some Of My Work</h1>
+                <p>These are few of my works on Web Development</p>
+                <div className="project_list">
+                    <ul>
+                        {
+                            works.map((work, index) => (
+                                <li>
+                                    <h3>{work.name}</h3>
+                                    <img src={work.image} alt={work.name}/>
+                                    <p>Visit: <a>{work.url}</a></p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
